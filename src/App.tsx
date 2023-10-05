3dd0627c6d555a132b07962e465b1e5549cdb5ea
Uğur Demirer <osmanugurdemirer@gmail.com>
2023-10-05 19:34:29 +0300

    conditional redirect added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route,Navigate} from 'react-router-dom'
 import ErrorPage from "./errorPage";
 import Home from "./pages/Home";
 function App() {
-
+  const loggedIn = true;
   return (
     <>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={ loggedIn ? (<Home />): (<Navigate replace to={"*"}/>)} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-bold underline">
-        Hello world!
-      </h1>
-      <div className="text-3xl text-bold">test</div>
+      <div>homepage</div>
     </>
   )
 }
