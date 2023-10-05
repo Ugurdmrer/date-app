837a634aff2f41af8a56fc3a985b44eca96b8f94
Uğur Demirer <osmanugurdemirer@gmail.com>
2023-10-05 19:43:39 +0300

    login and register paths are desinged.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route,Navigate} from 'react-router-dom'
 import ErrorPage from "./errorPage";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   const loggedIn = true;
   return (
     <>
         <Routes>
-          <Route path="/" element={ loggedIn ? (<Home />): (<Navigate replace to={"*"}/>)} />
+          <Route path="/login" element={ loggedIn ? (<Login />): (<Navigate replace to={"/"}/>)}/>
+          <Route path="/register" element={ loggedIn ? (<Register />): (<Navigate replace to={"/"}/>)}/>
+          <Route path="/" element={ loggedIn ? (<Home />): (<Navigate replace to={"/login"}/>)} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
     </>
